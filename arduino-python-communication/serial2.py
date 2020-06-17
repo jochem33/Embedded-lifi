7804ff34b29e3e38c096e667a975ca9b207773d1
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2020-06-17 13:44:24 +0200

    [change] found out data conversion in first step was the problem so now data is hardcoded

--- arduino-python-communication/serial2.py
+++ arduino-python-communication/serial2.py
@@ -7,12 +7,15 @@ file1 = open('output.txt', 'w+')
 file1.write("") 
 file1.close() 
 
+lineEndChar = ['1', '0', '1', '0', '0', '1', '0', '1']
+lineEndCharChar = "¥"
+
 print ('\n\n\n')
 
-sPort = '/dev/cu.usbmodem141101'
+sPort = '/dev/cu.usbmodem141201'
 
 aSerialData = serial.Serial(sPort,115201)
-
+9
 character = []
 characterStr = ""
 
@@ -25,18 +28,21 @@ def synchronize(syncChar):
         if (aSerialData.inWaiting()>0):
             sData = aSerialData.readline()
             bit = str(sData)[2]
+            # print(bit)
             syncList.append(bit)
+            # print(syncList[-8:])
+            # if(syncList[-8:] == syncChar and syncList[-1] == syncChar and synced == False):
             if(syncList[-8:] == syncChar and synced == False):
                 print("Synchronized!\n__________________________________")
                 synced = True
-                text = ["\x00"]
+                text = []
 
 
-text = ["\x00"]
+text = [""]
 
 print("Waiting for signal...\n")
 
-synchronize(['0', '1', '0', '1', '1', '1', '1', '1'])
+synchronize(lineEndChar)
 
 
 currentOkayCount = 0
@@ -52,26 +58,30 @@ while True:
         if (len(character) == 8):
             # print(characterStr.join(character), chr(int(characterStr.join(character), 2)))
             letter = chr(int(characterStr.join(character), 2))
-            print(letter)
+            # print(letter)
+            textStr = ""
             text.append(letter)
+            textStr = textStr.join(text)
+            print(textStr)
             character = []
             characterStr = ""
 
         if (len(text) >= 16):
             textStr = ""
             textStr = textStr.join(text)
-            if(textStr[0] == "_" and textStr[-0] == "_"):
+            print("_" + textStr[-0] + "__" + textStr[0] + "_")
+            if(textStr[0] == lineEndCharChar and textStr[-0] == lineEndCharChar):
                 currentOkayCount+= 1
                 totalOkayCount+= 1
 
-                file1 = open('output.txt', 'a') 
+                file1 = open('output.txt' + "\n", 'a') 
                 file1.write(textStr) 
                 file1.close() 
 
                 print(textStr, currentOkayCount, totalOkayCount)
             else:
-                synchronize(['0', '1', '0', '1', '1', '1', '1', '1'])
-                text = ["\x00"]
+                synchronize(lineEndChar)
+                text = [""]
                 currentOkayCount = 0
             text = []
 
